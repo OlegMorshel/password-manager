87b258bb17ea74f178e1df5d80c2e0d827ee0baa
Oleg <morshel@addamant.ru>
2022-08-07 05:10:39 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,8 @@ import classNames from 'classnames/bind';
 import React from 'react';
 import styles from './App.module.scss';
 const cnb = classNames.bind(styles);
-import { LogoSvg } from './assets/Icons';
 function App() {
-  return (
-    <div className={cnb('App')}>
-      <header className={cnb('App-header')}>
-        <div className={cnb('icon')}>
-          <LogoSvg />
-        </div>
-        <p className={cnb('text')}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className={cnb('App-link')} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className={cnb('App')}>Template</div>;
 }
 
 export default App;
