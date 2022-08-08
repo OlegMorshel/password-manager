cc5824710cdb205efe3ea1a48d60cb55b0d05cb7
Oleg <morshel@addamant.ru>
2022-08-08 05:57:54 +0300

    collection section

--- src/pages/MainPageWrapper/components/PasswordCollections/PasswordCollections.tsx
+++ src/pages/MainPageWrapper/components/PasswordCollections/PasswordCollections.tsx
@@ -1,9 +1,16 @@
+import Typography from '@src/components/UiKit/Typography/Typography'
 import classNames from 'classnames/bind'
 import React from 'react'
 import styles from './PasswordCollections.module.scss'
 const cnb = classNames.bind(styles)
 const PasswordCollections: React.FC = () => {
-  return <div>PasswordCollections</div>
+  return (
+    <div className={cnb('passwordCollectionsWrapper')}>
+      <Typography tag="h5" className={cnb('title')}>
+        Let's <span>secure</span> your details...
+      </Typography>
+    </div>
+  )
 }
 
 export default PasswordCollections
