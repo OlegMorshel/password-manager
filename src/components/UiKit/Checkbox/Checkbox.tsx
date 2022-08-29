f19732089e355dffaa3314682b9ac0196fffbe64
Oleg <93373269+OlegMorshel@users.noreply.github.com>
2022-08-29 13:09:44 +0300

    feat: format all project

--- src/components/UiKit/Checkbox/Checkbox.tsx
+++ src/components/UiKit/Checkbox/Checkbox.tsx
@@ -1,26 +1,21 @@
-import React, { memo } from "react";
-import styles from "./Checkbox.module.scss";
-import classNames from "classnames/bind";
-import Typography from "../Typography/Typography";
-const cnb = classNames.bind(styles);
+import React, { memo } from 'react'
+import styles from './Checkbox.module.scss'
+import classNames from 'classnames/bind'
+import Typography from '../Typography/Typography'
+const cnb = classNames.bind(styles)
 
 export interface CheckboxProps extends React.HTMLProps<HTMLInputElement> {
-  label?: string;
-  type?: "default" | "table";
+  label?: string
+  type?: 'default' | 'table'
 }
 
 const Checkbox: React.FC<CheckboxProps> = memo(({ label, type, id, ...props }) => (
-  <div className={cnb("wrapper")}>
-    <input
-      id={id}
-      type='checkbox'
-      className={cnb("defaultCheckbox", { tableType: type === "table" && !!!props.checked })}
-      {...props}
-    />
-    <Typography tag='p3' htmlFor={id}>
+  <div className={cnb('wrapper')}>
+    <input id={id} type="checkbox" className={cnb('defaultCheckbox', { tableType: type === 'table' && !!!props.checked })} {...props} />
+    <Typography tag="p3" htmlFor={id}>
       {label}
     </Typography>
   </div>
-));
+))
 
-export default Checkbox;
+export default Checkbox
